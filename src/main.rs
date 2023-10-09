8aac2981d7f1152c4b92a12c1e5db06378818ed3
blek <me@blek.codes>
2023-10-10 01:02:45 +1000

    render the QR code

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use warp::Filter;
+use qrcode::QrCode;
+use image::{Luma, png::PngEncoder};
+
+use warp::{Filter, reply::Response};
 
 #[tokio::main]
 async fn main() {
@@ -11,7 +14,21 @@ async fn main() {
                     .and(
                         warp::get()
                     )
-                    .map(|x| format!("u get {x}"))
+                    .map(|x| {
+                            let code = QrCode::new(x).unwrap();
+                            let img = code.render::<Luma<u8>>().build();
+
+                            let mut out = Vec::<u8>::new();
+                            let enc = PngEncoder::new(&mut out);
+
+                            let (width, height) = (img.width(), img.height());
+
+                            enc.encode(&img.into_raw(), width, height, image::ColorType::L8).unwrap();
+                            
+                            let mut res = Response::new(out.into());
+                            res.headers_mut().insert("Content-Type", warp::http::HeaderValue::from_static("image/png"));
+                            res
+                    })
             );
 
     warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
